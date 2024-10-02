2e74e95979441f9fc25c9b89b25776e5f0007673
hacki <work24to7@gmail.com>
2024-10-02 18:07:35 +0530

    Routing Issue fixed

--- ecom-ui/src/app/app.module.ts
+++ ecom-ui/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-import { HomeModule } from './features/home/home.module';
 import { SharedModule } from './shared/shared.module';
 import { RouterModule } from '@angular/router';
 
@@ -18,11 +17,9 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    HomeModule,
     RouterModule, 
   ],
   providers: [
-    provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
