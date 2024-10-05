f6e54b58192e2b302212c2825d02b73190f2e067
hackie <work24to7@gmail.com>
2024-10-06 01:49:27 +0530

    Admin sidebar routing added and CSS issue fixed

--- ecom-ui/src/app/shared/components/admin/sidebar/sidebar.component.ts
+++ ecom-ui/src/app/shared/components/admin/sidebar/sidebar.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class AdminSidebarComponent {
+isDropdownOpen: { [key: string]: boolean } = {
+    ecommerce: false,
+    category: false,
+    attributes: false,
+    order: false,
+    user: false,
+    location: false
+  };
 
+  toggleDropdown(evt: any, menu: string): void {
+    evt.preventDefault(); 
+    // Close other dropdowns
+    for (const key in this.isDropdownOpen) {
+      if (key !== menu) {
+        this.isDropdownOpen[key] = false;
+      }
+    }
+    // Toggle the selected menu
+    this.isDropdownOpen[menu] = !this.isDropdownOpen[menu];
+  }
 }
