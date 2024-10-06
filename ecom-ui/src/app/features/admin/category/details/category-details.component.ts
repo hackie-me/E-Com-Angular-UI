b4e946b412acd2ed604bf1ea9fb622bda4a8eb81
hackie <work24to7@gmail.com>
2024-10-06 15:51:33 +0530

    working on common grid

--- ecom-ui/src/app/features/admin/category/details/category-details.component.ts
+++ ecom-ui/src/app/features/admin/category/details/category-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import Breadcrumb from '../../../../shared/interfaces/bread-crump';
 
 @Component({
   selector: 'app-category-details',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './category-details.component.css'
 })
 export class CategoryDetailsComponent {
+
+  title: string = '';
+  action: string = 'Create';
+
+  breadcrumbs: Breadcrumb[] = [];
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentUrl = event.urlAfterRedirects || event.url;
+        console.log('Current URL:', currentUrl);
+        this.title = currentUrl.includes('create') ? 'Create New Category' : '';
+        this.title = currentUrl.includes('edit') ? 'Edit Category' : '';
+        this.title = currentUrl.includes('view') ? 'Category Details' : '';
+        this.action = currentUrl.includes('edit') ? 'Edit' : '';
+        this.action = currentUrl.includes('create') ? 'Create' : '';
+        this.action = currentUrl.includes('view') ? 'View' : '';
+        this.breadcrumbs = [
+          { label: 'Dashboard', url: '/admin/dashboard' },
+          { label: 'Categories', url: '/admin/categories' },
+          { label: this.action },
+        ];
+      }
+    });
+  }
 
 }
